1f81b4e1d19c60c58de4909a5bd8f181c150d0e8
17281 <17281@burnside.school.nz>
2020-05-21 11:28:25 +1200

    Introduce login+logout

--- project.py
+++ project.py
@@ -3,32 +3,49 @@
 Harry .C 
 Project:webapp
 '''
-from flask import Flask,render_template,request,g,redirect
+from flask import Flask, render_template, request, g, redirect, url_for
 import sqlite3
 
+DATABASE = 'User.db'
 app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
-
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 database = None
 #home page for the users
-@app.route ('/')
+@app.route('/')
 def home():
+    cursor = get_db().cursor()
+    error = None
+
     return render_template("index.html")
 
 #Admin login 
-@app.route ('/login', methods=['GET,' 'POST'])
+@app.route('/login' , methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        #if the password or username doesn't ==
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Password or Username, Please try again.'
-        else:
-            #if all correct
-            return redirect(url_for('index.html'))
+            error = 'Invalid Username or Password, please try again'
+        else: 
+            session['logged_in'] = True
+            return redirect(url_for('home'))
     return render_template("login.html", error=error)
+
+@app.route('/logout')
+    def logout():
+       session.pop('logged_in', None)
+       return redirect(url_for('home')) 
 
 #Debuging incase of error
 if __name__ == '__main__':
