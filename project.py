d09e55bd238fcc5d81457ba99f654534440bbcfa
17281 <17281@burnside.school.nz>
2020-05-26 08:42:50 +1200

    Added viewable data

--- project.py
+++ project.py
@@ -9,23 +9,11 @@ from functools import wraps
 
 app = Flask(__name__)
 app.secret_key = "my precious"
-database = None
-DATABASE = ''
+app.database = "Menu.db"
 
-#Data base connection 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-#-----------------------------------------------------------------------------
-
+def connect_db():
+    return sqlite3.connect(app.database)
 
 def login_required(f):
     @wraps(f)
@@ -47,12 +35,19 @@ def index():
 def home():
     #if session['logged_in'] in session: 
         #CHANGE stuff only if admin logs in-------
-    return render_template("shop.html")
+    g.db = connect_db() #estabilshes database connection
+    cur = g.db.execute('select * FROM Food')
+    posts = [dict(Name=row[0], Description=row[1]) for row in cur.fetchall()]
+    return render_template("shop.html", posts=posts) #renders a templet 
 
 
 #Admin login 
 @app.route('/login' , methods=['GET', 'POST'])
 def login():
+    #if user already logged in
+    if 'logged_in' in session:
+            flash('You have logged in already')
+            return redirect(url_for('home'))
     error = None
     if request.method == 'POST':
         #if user is correct then gain log in key
@@ -60,9 +55,11 @@ def login():
             error = 'Invalid Username or Password, please try again'
         else: 
             session['logged_in'] = True
-            flash('You are logged in as Admin')
-            return redirect(url_for('home'))
-    return render_template("login.html", error=error)
+            flash('You are logged in as Admin') #tell user that they logged in
+            return redirect(url_for('home')) #Move to home if admin is true
+        
+        
+    return render_template("login.html", error=error) #renders the templet
 
 #Logout
 @app.route('/logout')
@@ -70,8 +67,8 @@ def login():
 def logout():
     #removes login key
     session.pop('logged_in', None)
-    flash('You have logged out')
-    return redirect(url_for('index')) 
+    flash('You have logged out') #tells user that they logged out
+    return redirect(url_for('index')) #send user back to index page
 
 #Debuging incase of error
 if __name__ == '__main__':
