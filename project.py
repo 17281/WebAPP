a69b6a506d8b8eb596f041e87bfd5c621884df23
17281 <17281@burnside.school.nz>
2020-07-21 08:34:40 +1200

    Added choosing comments for selected item

--- project.py
+++ project.py
@@ -18,7 +18,8 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
-#Close the Dadtbase
+
+#Close the Dadtbase if database doesn't connect
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
@@ -41,14 +42,26 @@ def login_required(f):
 #index page
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html")#Not needed
+#================================================================================================
+
+@app.route('/fill/<int:post_id>', methods=['GET', 'POST'])
+def fill(post_id):
+    
+    #display all comments from User
+    cursor = get_db().cursor()
+    sql = ("SELECT Comments, CommentID, FoodID FROM User WHERE FoodID = ?")#selects the selected items where foodid = pic id
+    cursor.execute(sql,(post_id,))
+    
+    results = cursor.fetchall()
+    return render_template("comment.html", results=results)
 
 #Home Page
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     #display all data from menu
     cursor = get_db().cursor()
-    sql = ("SELECT Food.Name, Food.Description, User.Comments, User.CommentID, User.FoodID FROM Food JOIN User ON Food.ID = User.FoodID")
+    sql = ("SELECT Food.Name, Food.Description, User.Comments, User.CommentID, User.FoodID FROM Food JOIN User ON Food.ID = User.FoodID")#selects the selected items where the Food id = Forein key
     cursor.execute(sql)
     #reults == database
     results = cursor.fetchall()
@@ -62,12 +75,13 @@ def home():
 def login():
     #if user already logged in then stop them
     if 'logged_in' in session:
-            flash('You have logged in already')
-            return redirect(url_for('home'))
+            flash('You have logged in already')#tells user that they logged in
+            return redirect(url_for('home'))#redirect them to home page
     error = None
     if request.method == 'POST':
         #if user is correct then gain log in key
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':#required correct input from user
+            #If user input doesn't match up
             error = 'Invalid Username or Password, please try again'
         else: 
             session['logged_in'] = True
@@ -94,9 +108,9 @@ def delete():
         if request.method == "POST":
             #get item and delete with data base
             cursor = get_db().cursor()
-            id = int(request.form["item_name"])
+            id = int(request.form["item_name"])#Not Functional----------------
             if id == "":
-                flash('No comments left')
+                flash('No comments left')#------------------------------------
             else:
                 sql = ("DELETE FROM User WHERE CommentID=?")
                 #Delete comments where id == selected delete button
@@ -112,13 +126,13 @@ def add():
         #adds comments into table
         cursor = get_db().cursor()
         new_name = request.form["Comment"]
-        if new_name == "":
-            flash('No')
+        if new_name == "":#IF user tries to add Blanks into database
+            flash('No')#NO
         else:
             food_ID = int(request.form["FoodID"])
             #when the FoodID matches the texts ID
             sql = "INSERT INTO User (Comments,FoodID) VALUES (?,?)"
-            cursor.execute(sql,(new_name,food_ID))
+            cursor.execute(sql,(new_name,food_ID))#Execute sql
             get_db().commit()
     return redirect('/home')
 
